d2c1a64b70c5ca8a0075b12a2478578c1bdf12f6
Elizabeth Carranza <elizabeth.m.carranza@gmail.com>
2016-08-29 16:30:29 -0700

    complete

--- app/js/app.js
+++ app/js/app.js
@@ -4,13 +4,13 @@ var app = angular.module('myApp', []);
 app.directive('megaVideo', function ($sce) {
     return {
         restrict: 'E',
-        templateUrl: 'app/partials/megavideo.html',
-        //by setting the $scope to true, we are creating a new scope
-        $scope: true,
-        link: function ($scope, element, attrs) {
+        templateUrl: 'partials/megavideo.html',
+        //by setting the scope to true, we are creating a new scope
+        scope: true,
+        link: function (scope, element, attrs) {
             var videoPlayer = element.find('video')[0];
             //initialize and empty array of sources on scope.sources
-            $scope.sources = [];
+            scope.sources = [];
             // whitelist of video formats accepted
             function processSources() {
                 var sourceTypes = {
@@ -27,7 +27,7 @@ app.directive('megaVideo', function ($sce) {
 
                 for (source in sourceTypes) {
                     if (attrs.hasOwnProperty(source)) {
-                        $scope.sources.push({
+                        scope.sources.push({
                             type: sourceTypes[source].type,
                             src: $sce.trustAsResourceUrl(attrs[source])
                         });
@@ -36,22 +36,22 @@ app.directive('megaVideo', function ($sce) {
             }
             processSources();
 
-            $scope.video = {
+            scope.video = {
                 play: function () {
                     videoPlayer.play();
-                    $scope.video.status = 'play';
+                    scope.video.status = 'play';
                 },
                 pause: function () {
                     videoPlayer.pause();
-                    $scope.video.status = 'pause';
+                    scope.video.status = 'pause';
                 },
                 stop: function () {
                     videoPlayer.pause();
                     videoPlayer.currentTime = 0;
-                    $scope.video.status = 'stop';
+                    scope.video.status = 'stop';
                 },
                 togglePlay: function () {
-                    $scope.video.status == 'play' ? this.pause() : this.play();
+                    scope.video.status == 'play' ? this.pause() : this.play();
                 },
                 width: attrs.width,
                 height: attrs.height
